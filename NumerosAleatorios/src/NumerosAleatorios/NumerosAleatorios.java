a0b970f276d3a533d02bc235154589d37ecfe825
leninraul <leninraul@9ab54a18-b7f7-0680-6142-e3c1e8290400>
2012-06-15 14:16:35 +0000

    Primera versión First version Premiere version

---
+++ NumerosAleatorios/src/NumerosAleatorios/NumerosAleatorios.java
@@ -0,0 +1,10 @@
+package NumerosAleatorios;
+
+public class NumerosAleatorios {
+
+    public static void main(String[] args) {
+        //objeto para llamar al metodo que va ejecutar las permutaciones
+        CalcularTiempo ct= new CalcularTiempo();
+        ct.presentarTiempo();
+    }
+}
